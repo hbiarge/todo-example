2f3874ad55fdaea87d3a146af0ad9ef64e1b6aa6
Hugo Biarge <hugo@arosbi.com>
2014-11-28 15:55:45 +0100

    WebApi + comunicación con $http

---
+++ TodosApp/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace TodosApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
---
+++ TodosApp/app/controllers/todosCtrl.js
@@ -0,0 +1,56 @@
+﻿(function () {
+    'use strict';
+
+
+
+    function todosCtrl($scope, todoStore) {
+
+        $scope.newTodoText = '';
+
+        $scope.remaining = todoStore.remaining;
+
+        $scope.todos = [];
+
+        $scope.addNew = function () {
+            todoStore.addNew($scope.newTodoText).then(
+                function success(data) {
+                    $scope.todos.push(data);
+                    $scope.newTodoText = '';
+                },
+                function error() {
+                    // Show error
+                });
+            $scope.newTodoText = '';
+        };
+
+        $scope.removeDoneTodos = function () {
+            todoStore.removeDone().then(
+               function success() {
+                   $scope.todos = $scope.todos.filter(function (todo) {
+                       return todo.done !== true;
+                   });
+               },
+               function error() {
+                   // Show error
+               });
+        };
+
+        $scope.updateRemainig = function (todo) {
+            todoStore.switchDone(todo);
+        };
+
+        todoStore.getAll().then(
+            function success(data) {
+                $scope.todos = data;
+            },
+            function error() {
+                // Show error 
+            });
+
+    }
+
+    todosCtrl.$inject = ['$scope', 'todoStore'];
+
+    angular.module('todo').controller('TodosCtrl', todosCtrl);
+
+})();
