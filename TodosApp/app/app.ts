b5e77e870dd365c7a4b3f36075108955ecab7db6
hbiarge <hugo@arosbi.com>
2015-01-04 21:55:08 +0100

    app

---
+++ TodosApp/app/app.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../scripts/typings/angularjs/angular-route.d.ts" />
+
+((): void => {
+    "use strict";
+
+angular.module("todo", ["ngRoute"])
+    .config(["$routeProvider", ($routeProvider: ng.route.IRouteProvider) => {
+        
+        $routeProvider
+            .when("/", {
+                controller: "TodosCtrl", 
+                templateUrl: "app/tpl/List.html"
+            })
+            .when("/detail/:todoId", {
+                controller: "DetailCtrl", 
+                templateUrl: "app/tpl/Detail.html"
+            })
+            .otherwise({
+                redirect: "/"
+            });
+
+    }]);
+
+})();
